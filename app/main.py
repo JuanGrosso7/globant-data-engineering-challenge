4e4112fa734851266dac1013cbec440fcae0351b
JuanGrosso7 <jp7.grosso@gmail.com>
2025-07-23 00:59:17 -0300

    Initial FastAPI setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from .models import Department, Job, Employee
+from .routers import csv_router
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(csv_router.router)
+
+
 @app.get("/")
 async def root():
+
     return {"message": "Globant API success"}
